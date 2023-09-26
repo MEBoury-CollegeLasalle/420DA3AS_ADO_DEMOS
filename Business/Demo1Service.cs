eb403467f0598a865db2e51f6aca0df924c615c8
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-09-26 07:53:51 -0400

    created business pseudo-class

---
+++ Business/Demo1Service.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPatates.Business;
+internal class Demo1Service {
+
+}
