6ca955e35d498c2277ea892c3625d36cc5b26a74
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-09-26 15:33:09 -0400

    small fixes to Demo2DTO

--- DataAccess/Demo2DTO.cs
+++ DataAccess/Demo2DTO.cs
@@ -8,21 +8,18 @@ namespace TestPatates.DataAccess;
 internal class Demo2DTO {
     private Demo1DTO? demo1Object;
 
-
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
     public string? Description { get; set; }
 
-    public int ObjetDemo1Id { get; set; }
+    public int Demo1Id { get; set; }
 
     public Demo1DTO ObjetDemo1 { 
         get {
             // TODO: exemple de lazy loading (pas un vrai TODO)
-            if (this.demo1Object == null) {
-                this.demo1Object = Demo1DAO.GetById(this.ObjetDemo1Id);
-            }
+            this.demo1Object ??= Demo1DAO.GetById(this.Demo1Id);
             return this.demo1Object;
         } 
         set { 
